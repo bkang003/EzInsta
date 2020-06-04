93b46e0f0f7a16455d40ced93db5f619dc977eb0
Bryan Kang <bkang003@gmail.com>
2020-06-03 22:59:37 -0400

    basic CRUD for posts

--- Insta/urls.py
+++ Insta/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
 # from . import views
-from Insta.views import PostListView
+from Insta.views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', PostListView.as_view()),
+    path('', PostListView.as_view(), name='posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='make_post'),
+    path('post/edit/<int:pk>/', PostUpdateView.as_view(), name='edit_post'),
+    path('post/delete/<int:pk>', PostDeleteView.as_view(), name='delete_post'),
 ]
--- Insta/views.py
+++ Insta/views.py
@@ -1,13 +1,39 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 # Create your views here.
-
-# class HelloDjango(TemplateView):
-#     template_name = 'home.html'
-
 
 class PostListView(ListView):
     model = Post
     template_name = 'home.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = '__all__'
+    # def blog_detail_view(request, primary_key):
+    #     post = Post.objects.get(pk=primary_key)
+    #     except Post.DoesNotExist:
+    #         raise Http404('Post does not exist')
+
+    #     return render(request, 'post_detail.html', context={'post': post})
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title']
+    template_name = 'post_edit.html'
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy("posts") # needs to match name in urlpatterns
